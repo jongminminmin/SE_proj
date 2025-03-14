244e4f32e28a4fdb422be78cc2fbaf62337442e6
jongminminmin <wedf5633@gmail.com>
2025-03-14 16:31:34 +0900

    추가 작성 필요

---
+++ src/main/java/ac/kr/changwon/se_proj/Controller/LoginController.java
@@ -0,0 +1,12 @@
+package ac.kr.changwon.se_proj.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* 로그인 컨트롤러
+* 요청 시 로그인 페이지로 이동 */
+@RestController
+public class LoginController {
+
+}
