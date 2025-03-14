0063004949e58ea875eb12dc83220e1a42dc7a82
jongminminmin <wedf5633@gmail.com>
2025-03-14 18:28:40 +0900

    요청 처리 분리 작업

--- src/main/java/ac/kr/changwon/se_proj/Controller/LoginController.java
+++ src/main/java/ac/kr/changwon/se_proj/Controller/LoginController.java
@@ -8,5 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 * 요청 시 로그인 페이지로 이동 */
 @RestController
 public class LoginController {
+    
+    /* 로그인 화면 관련 컨트롤러*/
+    @Autowired
+    private LoginService loginService;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        if(loginService.authenticate(loginRequest.getId(),loginRequest.getPassword())) {
+            return ResponseEntity.ok(loginRequest);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid ID/Password");
+        }
+    }
 
 }
