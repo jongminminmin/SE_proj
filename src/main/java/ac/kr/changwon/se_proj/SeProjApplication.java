d8d90c4bd7f4a89dc2fdb56bc897dd015dfc56dd
jongminminmin <wedf5633@gmail.com>
2025-03-26 21:32:15 +0900

    전체 패키지 범위로 프로젝트 범위 수정

--- src/main/java/ac/kr/changwon/se_proj/SeProjApplication.java
+++ src/main/java/ac/kr/changwon/se_proj/SeProjApplication.java
@@ -2,8 +2,7 @@ package ac.kr.changwon.se_proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ac.kr.changwon.se_proj" )
 public class SeProjApplication {
 
     public static void main(String[] args) {
