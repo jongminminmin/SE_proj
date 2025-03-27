2c3257ed5b28f669b435c7962af6eaa463614f0a
jongminminmin <wedf5633@gmail.com>
2025-03-27 14:57:02 +0900

    필요없는 import문 삭제

--- src/main/java/ac/kr/changwon/se_proj/Controller/MainController.java
+++ src/main/java/ac/kr/changwon/se_proj/Controller/MainController.java
@@ -1,9 +1,7 @@
 package ac.kr.changwon.se_proj.Controller;
 
-import ac.kr.changwon.se_proj.Model.Project;
 import ac.kr.changwon.se_proj.Service.Interface.ProjectService;
 import ac.kr.changwon.se_proj.Service.Interface.TaskService;
-import ac.kr.changwon.se_proj.Service.Interface.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
