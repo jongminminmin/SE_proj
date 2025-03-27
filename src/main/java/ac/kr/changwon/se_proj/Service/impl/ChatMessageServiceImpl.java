1985978d207e7ff06b02421e146c365296053c27
jongminminmin <wedf5633@gmail.com>
2025-03-27 14:56:12 +0900

    채팅 프로세스 구현체 메서드

--- src/main/java/ac/kr/changwon/se_proj/Service/impl/ChatMessageServiceImpl.java
+++ src/main/java/ac/kr/changwon/se_proj/Service/impl/ChatMessageServiceImpl.java
@@ -1,18 +1,25 @@
 package ac.kr.changwon.se_proj.Service.impl;
 
 import ac.kr.changwon.se_proj.Model.ChatMessage;
+import ac.kr.changwon.se_proj.Model.User;
 import ac.kr.changwon.se_proj.Repository.ChatMessageRepository;
+import ac.kr.changwon.se_proj.Repository.UserRepository;
 import ac.kr.changwon.se_proj.Service.Interface.ChatMessageService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+
 @Service
 public class ChatMessageServiceImpl implements ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final UserRepository userRepository;
 
-    public ChatMessageServiceImpl(ChatMessageRepository chatMessageRepository) {
+    public ChatMessageServiceImpl(ChatMessageRepository chatMessageRepository, UserRepository userRepository) {
         this.chatMessageRepository = chatMessageRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -34,6 +41,23 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     @Override
     public void deleteById(Integer id) {
         chatMessageRepository.deleteById(id);
+    }
+
+    @Override
+    public void process(ChatMessage chatMessage) {
+        User sender = userRepository.findById(String.valueOf(chatMessage.getSender())).orElse(null);
+        if (sender == null) {
+            return;
+        }
+
+        ChatMessage message = new ChatMessage(
+                sender,
+                chatMessage.getReceiverId(),
+                chatMessage.getContent(),
+                chatMessage.getTimestamp() != null ? chatMessage.getTimestamp() : LocalDateTime.now()
+        );
+
+        chatMessageRepository.save(message);
     }
 
 }
