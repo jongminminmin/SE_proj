efbe85f58ca0c864baf4f8a890f8883ddf25cfee
jongminminmin <wedf5633@gmail.com>
2025-03-27 14:55:25 +0900

    채팅 서비스 DTO 클래스

---
+++ src/main/java/ac/kr/changwon/se_proj/dto/ChatMessageDTO.java
@@ -0,0 +1,13 @@
+package ac.kr.changwon.se_proj.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ChatMessageDTO {
+    private String senderId;
+    private String receiverId;
+    private String message;
+    private LocalDateTime timestamp;
+}
