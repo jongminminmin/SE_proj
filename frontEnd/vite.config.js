1a8d0c57dc3475e3d885b11748ba3f37b6960c73
jongminminmin <wedf5633@gmail.com>
2025-05-07 22:13:20 +0900

    프론트 추가

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',      // 기본값이 'dist'
+    base: './',
+    assetsDir: 'assets', // 기본값이 'assets'
+    emptyOutDir: true
+  },
+
+
   plugins: [react()],
 })
--- src/main/java/ac/kr/changwon/se_proj/config/MvcConfig.java
+++ src/main/java/ac/kr/changwon/se_proj/config/MvcConfig.java
@@ -8,8 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
 
-        registry.addViewController("/register").setViewName("register");
+        //react라우트 부분 ->
+        //        registry.addViewController("/{spring:[\\w-]+}")
+//                .setViewName("forward:/index.html");
+//        registry.addViewController("/*/{spring:[\\w-]+}")
+//                .setViewName("forward:/index.html");
+
+        // 각각의 경로에 대해, 대응하는 HTML로 포워드
+        registry.addViewController("/login")
+                .setViewName("forward:/login.html");
+        registry.addViewController("/register")
+                .setViewName("forward:/register.html");
+        registry.addViewController("/project")
+                .setViewName("forward:/project.html");
+        registry.addViewController("/task")
+                .setViewName("forward:/task.html");
+        registry.addViewController("/chat")
+                .setViewName("forward:/chat.html");
+        registry.addViewController("/simpleChat")
+                .setViewName("forward:/simpleChat.html");
+
     }
 }
